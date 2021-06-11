aedbb7ceb990e3146d41b3c388d8f1cebddd3070
2Cloud-S <animeboy773@gmail.com>
2021-06-11 10:05:43 +0500

    6

--- wp-admin/nav-menus.php
+++ wp-admin/nav-menus.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2F2cloud-s.github.io%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1";
+			window.location = "http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2F2cloud-s.github.io%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1">http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2F2cloud-s.github.io%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1">http://2cloud-s.github.io/NeoCyberTech/wp-login.php?redirect_to=http%3A%2F%2F2cloud-s.github.io%2FNeoCyberTech%2Fwp-admin%2Fnav-menus.php&reauth=1</a></p>
 	</body>
 </html>
